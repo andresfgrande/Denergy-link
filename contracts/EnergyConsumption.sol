7508af5387db6578b9fa69c13bc1839e664c0b9e
Andrés Grande <andres.grande@somosinapsis.com>
2023-05-13 16:00:58 +0200

    Create contract for Energy consumption

---
+++ contracts/EnergyConsumption.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "https://github.com/bokkypoobah/BokkyPooBahsDateTimeLibrary/blob/master/contracts/BokkyPooBahsDateTimeLibrary.sol";
+
+contract EnergyConsumption {
+
+    
+}
