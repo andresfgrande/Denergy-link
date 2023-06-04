65604300ddf3614c87c13ac564cd73b4b94fd9aa
Andrés Grande <andres.grande@somosinapsis.com>
2023-06-04 16:11:05 +0200

    Added visual feedback for user in frontend to know if transaction is completed.

--- api-denergy/public/main.js
+++ api-denergy/public/main.js
@@ -1,7 +1,12 @@
+/**
+ * Simulation: call from IoT device for registering produced energy
+ */
 document.getElementById('energy-form').addEventListener('submit', function(event) {
     event.preventDefault();
 
     const energyProduced = document.getElementById('energyProduced').value;
+    const processingElement = document.getElementById('processing1');
+    processingElement.style.display = 'block';
 
     fetch('http://localhost:3000/registerEnergyProduction', {
         method: 'POST',
@@ -14,18 +19,29 @@ document.getElementById('energy-form').addEventListener('submit', function(event
     })
     .then(response => response.json())
     .then(data => {
+        //Show succes message in frontend and transaction data
         console.log('Success:', data);
+        alert('Transaction was successful! - ' + "tx hash: " +data.transactionHash);
+        processingElement.style.display = 'none'; 
     })
     .catch((error) => {
+        //Show success message in frontend and error data
         console.error('Error:', error);
+        alert('An error occurred. Please try again.', error);
+        processingElement.style.display = 'none'; 
     });
 });
 
+/**
+ * Simulation: call from IoT device for registering consumed energy
+ */
 document.getElementById('consumption-form').addEventListener('submit', function(event) {
     event.preventDefault();
 
     const consumerAddress = document.getElementById('consumerAddress').value;
     const consumedEnergy = document.getElementById('consumedEnergy').value;
+    const processingElement = document.getElementById('processing2');
+    processingElement.style.display = 'block';
 
     fetch('http://localhost:3000/registerEnergyConsumption', {
         method: 'POST',
@@ -40,9 +56,13 @@ document.getElementById('consumption-form').addEventListener('submit', function(
     .then(response => response.json())
     .then(data => {
         console.log('Success:', data);
+        alert('Transaction was successful! - ' + "tx hash: " +data.transactionHash);
+        processingElement.style.display = 'none'; 
     })
     .catch((error) => {
         console.error('Error:', error);
+        alert('An error occurred. Please try again.', error);
+        processingElement.style.display = 'none'; 
     });
 });
 
@@ -51,6 +71,8 @@ document.getElementById('production-test-form').addEventListener('submit', funct
 
     const energyProduced = document.getElementById('energyProducedTest').value;
     const timestampInput = document.getElementById('timestampTest').value;
+    const processingElement = document.getElementById('processing3');
+    processingElement.style.display = 'block';
     
     // Convert date string into timestamp in seconds
     const timestamp = Math.floor(new Date(timestampInput).getTime() / 1000);
@@ -68,9 +90,13 @@ document.getElementById('production-test-form').addEventListener('submit', funct
     .then(response => response.json())
     .then(data => {
         console.log('Success:', data);
+        alert('Transaction was successful! - ' + "tx hash: " +data.transactionHash);
+        processingElement.style.display = 'none'; 
     })
     .catch((error) => {
         console.error('Error:', error);
+        alert('An error occurred. Please try again.', error);
+        processingElement.style.display = 'none'; 
     });
 });
 
@@ -80,6 +106,8 @@ document.getElementById('consumption-test-form').addEventListener('submit', func
     const consumerAddress = document.getElementById('consumerAddressTest').value;
     const consumedEnergy = document.getElementById('consumedEnergyTest').value;
     const timestampInput = document.getElementById('timestampTestCon').value;
+    const processingElement = document.getElementById('processing4');
+    processingElement.style.display = 'block';
     
     // Convert date string into timestamp in seconds
     const timestamp = Math.floor(new Date(timestampInput).getTime() / 1000);
@@ -98,8 +126,12 @@ document.getElementById('consumption-test-form').addEventListener('submit', func
     .then(response => response.json())
     .then(data => {
         console.log('Success:', data);
+        alert('Transaction was successful! - ' + "tx hash: " +data.transactionHash);
+        processingElement.style.display = 'none'; 
     })
     .catch((error) => {
         console.error('Error:', error);
+        alert('An error occurred. Please try again.', error);
+        processingElement.style.display = 'none'; 
     });
 });
